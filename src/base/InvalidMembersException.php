94d3f7c874f541c5678b39f8ac50baed3a317ca2
Dong Chen <cdcchen@gmail.com>
2016-07-05 12:01:32 +0800

    Create InvalidMembersException

---
+++ src/base/InvalidMembersException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chendong
+ * Date: 16/3/9
+ * Time: 22:01
+ */
+ 
+namespace cdcchen\wechat\base;
+
+
+class InvalidMembersException extends \Exception
+{
+}
